4d1a84500a94db9cd2c7ef24b4d3584448a3e54c
Ayoub <aybelhaj@car2s2.42barcelona.com>
2025-01-06 15:40:04 +0100

    add todo

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aybelhaj <aybelhaj@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 11:21:21 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/01/05 16:32:22 by aybelhaj         ###   ########.fr       */
+/*   Updated: 2025/01/06 15:39:53 by aybelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "pushswap.h"
@@ -44,16 +44,12 @@ t_stack	*ft_lstnew1(int value)
 void	push(char **argv)
 {
 	int		i;
-	//t_list	*new;
-	//t_list	*new2;
 	t_stack	*stack_a = NULL;
-
 	t_stack *curr;
 	t_stack *root;
 
 	curr = NULL;
 	root = stack_a;
-	//a = new_stack();
 	i = 1;
 	while (argv[i] != NULL)
 	{
@@ -64,22 +60,16 @@ void	push(char **argv)
 		}
 		else
 		{
-				//curr->next->value = 5;
-				//printf("%d", curr->next->value);
 			curr->next = ft_lstnew1(ft_atoi(argv[i]));
 			curr = curr->next;
 		}
-			//ft_lstadd_back(&new, new2);
 		i++;
 	}
 	stack_a = root;
 	sa_swap(stack_a);
-//	a->top = new;
-//	a->size = ft_lstsize(new);
 	while (stack_a != NULL)
 	{
 		printf("%i\n", stack_a->value);
 		stack_a = stack_a->next;
 	}
-	//return ;
 }
