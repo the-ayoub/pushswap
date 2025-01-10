242b2e92842f8585f4c709f8f8075d1e056f4382
Ayoub <aybelhaj@car6s2.42barcelona.com>
2025-01-10 15:46:10 +0100

    add

--- order.c
+++ order.c
@@ -6,7 +6,7 @@
 /*   By: aybelhaj <aybelhaj@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 17:16:26 by aybelhaj          #+#    #+#             */
-/*   Updated: 2025/01/08 19:41:01 by aybelhaj         ###   ########.fr       */
+/*   Updated: 2025/01/10 14:14:06 by aybelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "pushswap.h"
@@ -19,24 +19,14 @@ void sortthree(t_stack **stack_a)
 	a = (*stack_a)->value;
 	b = (*stack_a)->next->value;
 	c = (*stack_a)->next->next->value;
-	/*if{
-	if(a>b && a>c && b>c)
-	{
+	if(a>c && c<b && a>b)
+		sa(*stack_a,1),rra(stack_a,1);
+	else if(b<a && a<c)
 		sa(*stack_a,1);
-		rra(stack_a,1);
-	}
-	else
-	{
+	else if(b<c && c<a)
 		ra(stack_a,1);
-	if(b>a && a>c)
-	{
+	else if(c<a && a<b)
 		rra(stack_a,1);
-	}
-	else if(b>a && a>c)
-	{
-		sa(*stack_a,1);
-		ra(stack_a,1);
-	}	
-	else
-		sa(*stack_a,1);*/
+	else if (c<b && b > a)
+		sa(*stack_a,1),ra(stack_a,1);
 }
